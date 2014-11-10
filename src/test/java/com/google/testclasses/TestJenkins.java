cdd6b13c5a5ae80a5900c717e6c3e8b2b954e359
Yudhvir Virk <udvdjm@gmail.com>
2014-11-10 09:23:05 +0530

    hello

---
+++ src/test/java/com/google/testclasses/TestJenkins.java
@@ -0,0 +1,13 @@
+package com.google.testclasses;
+
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+	
+	@Test
+	public void test() throws Exception{
+		System.out.println("Jenkins Test");
+	}
+
+}
